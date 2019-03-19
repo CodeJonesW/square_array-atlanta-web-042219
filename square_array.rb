f6c847857076020d70702139a8cb9e94c179e8aa
Will Jones <willjones2010@gmail.com>
2019-03-19 20:48:47 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,6 @@
 def square_array(array)
   newArray = []
-  array.each do |item|
-    newArray.push {|item| new_array.push(item**2)}
+  array.each do {|item| new_array.push(item**2)}
     return newArray
 end    
 end
